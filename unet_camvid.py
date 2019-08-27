382ea270b0c6c754f58bca849494cc2d82107b23
pm <piotr.mroczek@ocelotit.pl>
2019-08-27 21:29:28 +0200

    bum

--- unet_camvid.py
+++ unet_camvid.py
@@ -21,7 +21,7 @@ class myUnet(object):
 
     def get_unet(self):
 
-        help(merge)
+        # help(merge)
 
         inputs = Input((self.img_rows, self.img_cols, 3))
 
@@ -58,7 +58,7 @@ class myUnet(object):
 
         up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
             UpSampling2D(size=(2, 2))(drop5))
-        merge6 = merge([drop4, up6], mode='concat', concat_axis=3)
+        merge6 = merge.merge([drop4, up6], mode='concat', concat_axis=3)
         # print(up6)
         # print(merge6)
         conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
@@ -67,7 +67,7 @@ class myUnet(object):
         # print(conv6)
         up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
             UpSampling2D(size=(2, 2))(conv6))
-        merge7 = merge([conv3, up7], mode='concat', concat_axis=3)
+        merge7 = merge.merge([conv3, up7], mode='concat', concat_axis=3)
         # print(up7)
         # print(merge7)
         conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
@@ -76,13 +76,13 @@ class myUnet(object):
         # print(conv7)
         up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
             UpSampling2D(size=(2, 2))(conv7))
-        merge8 = merge([conv2, up8], mode='concat', concat_axis=3)
+        merge8 = merge.merge([conv2, up8], mode='concat', concat_axis=3)
         conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
         conv8 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv8)
 
         up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
             UpSampling2D(size=(2, 2))(conv8))
-        merge9 = merge([conv1, up9], mode='concat', concat_axis=3)
+        merge9 = merge.merge([conv1, up9], mode='concat', concat_axis=3)
         # print(up9)
         # print(merge9)
         conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
