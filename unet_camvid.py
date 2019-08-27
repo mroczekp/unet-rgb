311e83aeedaa22d8265ff02675b2b0d984c2fd43
pm <piotr.mroczek@ocelotit.pl>
2019-08-27 21:20:15 +0200

    comment out print statements

--- unet_camvid.py
+++ unet_camvid.py
@@ -24,25 +24,25 @@ class myUnet(object):
 
         conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
         # print(conv1)
-        print "conv1 shape:", conv1.shape
+        # print "conv1 shape:", conv1.shape
         conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
-        print "conv1 shape:", conv1.shape
+        # print "conv1 shape:", conv1.shape
         pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-        print "pool1 shape:", pool1.shape
+        # print "pool1 shape:", pool1.shape
 
         conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
-        print "conv2 shape:", conv2.shape
+        # print "conv2 shape:", conv2.shape
         conv2 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
-        print "conv2 shape:", conv2.shape
+        # print "conv2 shape:", conv2.shape
         pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-        print "pool2 shape:", pool2.shape
+        # print "pool2 shape:", pool2.shape
 
         conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
-        print "conv3 shape:", conv3.shape
+        # print "conv3 shape:", conv3.shape
         conv3 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
-        print "conv3 shape:", conv3.shape
+        # print "conv3 shape:", conv3.shape
         pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-        print "pool3 shape:", pool3.shape
+        # print "pool3 shape:", pool3.shape
 
         conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
         conv4 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
@@ -56,21 +56,21 @@ class myUnet(object):
         up6 = Conv2D(512, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
             UpSampling2D(size=(2, 2))(drop5))
         merge6 = merge([drop4, up6], mode='concat', concat_axis=3)
-        print(up6)
-        print(merge6)
+        # print(up6)
+        # print(merge6)
         conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
-        print(conv6)
+        # print(conv6)
         conv6 = Conv2D(512, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv6)
-        print(conv6)
+        # print(conv6)
         up7 = Conv2D(256, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
             UpSampling2D(size=(2, 2))(conv6))
         merge7 = merge([conv3, up7], mode='concat', concat_axis=3)
-        print(up7)
-        print(merge7)
+        # print(up7)
+        # print(merge7)
         conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
-        print(conv7)
+        # print(conv7)
         conv7 = Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv7)
-        print(conv7)
+        # print(conv7)
         up8 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
             UpSampling2D(size=(2, 2))(conv7))
         merge8 = merge([conv2, up8], mode='concat', concat_axis=3)
@@ -80,17 +80,17 @@ class myUnet(object):
         up9 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(
             UpSampling2D(size=(2, 2))(conv8))
         merge9 = merge([conv1, up9], mode='concat', concat_axis=3)
-        print(up9)
-        print(merge9)
+        # print(up9)
+        # print(merge9)
         conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
-        print(conv9)
+        # print(conv9)
         conv9 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
-        print(conv9)
+        # print(conv9)
         conv9 = Conv2D(12, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv9)
-        print "conv9 shape:", conv9.shape
+        # print "conv9 shape:", conv9.shape
 
         conv10 = Conv2D(12, 1, activation='softmax')(conv9)
-        print(conv10)
+        # print(conv10)
         model = Model(input=inputs, output=conv10)
 
         model.compile(optimizer=Adam(lr=1e-4), loss='categorical_crossentropy', metrics=['accuracy'])
